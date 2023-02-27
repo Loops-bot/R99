2571b34cdccb398b09b7d0645baf8668c5c01c44
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-02-26 22:47:49 -0600

    Update gp-rules.js

--- plugins/gp-rules.js
+++ plugins/gp-rules.js
@@ -2,19 +2,19 @@ let handler = async (m, { conn }) => {
 
 m.reply(`
 *┌──────────────────────── ◈*
-┃📜 *Normas del Grupo*
+┃ 📜 *Normas del Grupo*
 ┃
-┃✅ El tema principal son los Bots de WhatsApp pero se permite hacer preguntas sobre cualquier otro tema.
+┃ ✅ El tema principal son los Bots de WhatsApp pero se permite hacer preguntas sobre cualquier otro tema.
 ┃
-┃⚠️ No hacer spam: compartir links no autorizados, ads, cryptos, spam de comandos
+┃ ⚠️ No hacer spam: compartir links no autorizados, ads, cryptos, spam de comandos
 ┃
-┃⚠️ Respetar a los usuarios del grupo
+┃ ⚠️ Respetar a los usuarios del grupo
 ┃
-┃⚠️ No robar si están jugando al */slot*
+┃ ⚠️ No robar si están jugando al */slot*
 ┃
-┃🚫 Contenido o demás relacionado con +18 🔞
+┃ 🚫 Contenido o demás relacionado con +18 🔞
 ┃
-┃Incumplir las reglas significa podrías recibir un castigo o ser expulsado.
+┃ Incumplir las reglas significa podrías recibir un castigo o ser expulsado.
 *└──────────────────────── ◈*
 }
 

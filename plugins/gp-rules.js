5c194272b2f46649f4e378e88f1ff95850505abe
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-02-26 23:00:22 -0600

    Update gp-rules.js

--- plugins/gp-rules.js
+++ plugins/gp-rules.js
@@ -1,25 +1,27 @@
+
 let handler = async (m, { conn }) => {
 
 m.reply(`
-*┌──────────────────────── ◈*
-┃ *📜Normas del Grupo*
-┃
-┃ *✅El tema principal son los Bots de WhatsApp pero se permite hacer preguntas sobre cualquier otro tema.*
-┃
-┃ ⚠️No hacer spam: compartir links no autorizados, ads, cryptos, spam de comandos
-┃
-┃ ⚠️Respetar a los usuarios del grupo
-┃
-┃ ⚠️No robar si están jugando al */slot*
-┃
-┃ 🚫Contenido o demás relacionado con +18 🔞
-┃
-┃ *Incumplir las reglas significa podrías recibir un castigo o ser expulsado.*
-*└──────────────────────── ◈*
+≡  *Loopsᴮᴼᵀ ┃ RULES*
 
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
+▢ 📜 *Normas del Grupo*
+
+▢ ✅ El tema principal son los Bots de WhatsApp pero se permite hacer preguntas sobre cualquier otro tema.
+
+▢ ⚠️ No hacer spam: compartir links no autorizados, ads, cryptos, spam de comandos
+
+▢ ⚠️ Respetar a los usuarios del grupo
+
+▢ ⚠️ No robar si están jugando al */slot*
+
+▢ 🚫 Contenido o demás relacionado con +18 🔞
+
+▢ Incumplir las reglas significa podrías recibir un castigo o ser expulsado.
+◈ ━━━━━━━━━━━━━━━━━━━━ ◈
 }
 handler.help = ['rules']
 handler.tags = ['group']
-handler.command = ['rules']
+handler.command = ['rules', 'rule', 'Rules', 'RULES'] 
 
-export default handler 
+export default handler

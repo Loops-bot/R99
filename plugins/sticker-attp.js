59e3ee1df1302a9552c71323791501fadd482ba2
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-02-26 20:05:52 -0600

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -3,31 +3,31 @@ if (!text) throw `⚠️ *_Ingrese un texto._*`
 let teks = encodeURI(text)
 
 if (command == 'attp') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
     
 if (command == 'attp2') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
 
 if (command == 'ttp5') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
     
 if (command == 'ttp4') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
     
 if (command == 'ttp3') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp3?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
     
 if (command == 'ttp2') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp2?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
     
 if (command == 'ttp') {
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adsticker, null, script, m)
+await conn.sendNyanCat(m.chat, global.wait, packname, adsticker, null, script, m)
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: botname, body: `h`, mediaType: 2, sourceUrl: linkgc, thumbnail: miniurl}}}, { quoted: m })}
     
 }

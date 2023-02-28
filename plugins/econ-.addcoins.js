f222906d8500e5ec90988eb11e6f29c775ddc7ab
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-02-27 18:15:02 -0600

    Create econ-.addcoins.js

---
+++ plugins/econ-.addcoins.js
@@ -0,0 +1,30 @@
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, text }) => {
+  let who
+  if (m.isGroup) who = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw '✳️ Taguea al usuario'
+  let txt = text.replace('@' + who.split`@`[0], '').trim()
+  if (!txt) throw '✳️ Ingrese la cantidad de *coins* que quiere añadir'
+  if (isNaN(txt)) throw ' 🔢 sólo números'
+  let xp = parseInt(txt)
+  let exp = xp
+  
+  if (exp < 1) throw '✳️ Mínimo es  *1*'
+  let users = global.db.data.users
+  users[who].diamond += limit 
+
+  await m.reply(`≡ *COINS AÑADIDO*
+┌──────────────
+▢  *Total:* ${limit}
+└──────────────`)
+ conn.fakeReply(m.chat, `▢ Recibiste \n\n *+${limit} coins*`, who, m.text)
+}
+
+handler.help = ['addcoins <@user>']
+handler.tags = ['econ']
+handler.command = ['addcoins'] 
+handler.rowner = true
+
+export default handler

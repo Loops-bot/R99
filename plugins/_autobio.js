c9b594a8f6ef9f32cdb09b3eea6ef966e122d3aa
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-01 18:42:14 -0600

    Loops ᴮᴼᵀ ✨

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
+		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By r98 Biografía de mi owner ┃ Aveses me siento *mal* pero acomodo la silla y me siento *bien*`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 

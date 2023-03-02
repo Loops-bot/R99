a7673e3cf6e1fec704f97cd0fc68b95a29c05e93
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-01 19:51:55 -0600

    Update econ-daily.js

--- plugins/econ-daily.js
+++ plugins/econ-daily.js
@@ -7,12 +7,12 @@ const prem = 20000
 let handler = async (m, {conn, isPrems }) => {
   let time = global.db.data.users[m.sender].lastclaim + 86400000
   if (new Date - global.db.data.users[m.sender].lastclaim < 86400000) throw `🎁 *Ya recogiste tu recompensa diaria*\n\n🕚 Vuelve en *${msToTime(time - new Date())}* `
-  global.db.data.users[m.sender].exp += isPrems ? prem : free
+  global.db.data.users[m.sender].limit += isPrems ? prem : free
   m.reply(`
 🎁 *RECOMPENSA DIARIA*
 
 ▢ *Has recibido:*
-🆙 *XP* : +${isPrems ? prem : free}`)
+🆙 *COINS* : +${isPrems ? prem : free}`)
   global.db.data.users[m.sender].lastclaim = new Date * 1
 }
 handler.help = ['daily']

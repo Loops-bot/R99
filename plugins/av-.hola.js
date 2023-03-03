8ed907bc40d7d6ec84e4e8e0e1d8c533f2c69d02
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-02 21:14:23 -0600

    Update av-.hola.js

--- plugins/av-.hola.js
+++ plugins/av-.hola.js
@@ -1,16 +1,1 @@
 
-let handler = async (m, { conn}) => {
-
-let name = conn.getName(m.sender)
-
-conn.reply(m.chat, `Hola *${name}* soy ⍣Loops ᴮᴼᵀ࿐`)
-}
-
-handler.customPrefix = /^(hola)$/i
-handler.command = new RegExp
-
-export default handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}

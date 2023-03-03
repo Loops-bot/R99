ee8c4640ad4dc0831fe3d307d026edc28ad43b34
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-02 21:02:35 -0600

    Loops ᴮᴼᵀ ✨

--- plugins/av-.hola.js
+++ plugins/av-.hola.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
 
-conn.reply(m.chat, `Hola @${who.split("@s.whatsapp.net")[0]} soy ⍣Loops ᴮᴼᵀ࿐ `)
+conn.reply(m.chat, `Hola @{name} soy ⍣Loops ᴮᴼᵀ࿐`)
 }
 
 handler.customPrefix = /^(hola)$/i

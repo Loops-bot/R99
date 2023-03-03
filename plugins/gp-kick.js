9f5b63328c949c5a5545d5b68558211a035de28c
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-03 09:30:37 -0600

    ⍣Loops ᴮᴼᵀ࿐

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -3,17 +3,17 @@ let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
 let kickte = `✳️ Uso correcto del comamdo\n*${usedPrefix + command}* @tag`
 
-let av = './src/mp3/kick.mp3'
+
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
 m.reply(`✅ Usuario eliminado`) 
-	
+let av = './src/mp3/kick.mp3'	
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 	
-}
+
 handler.help = ['kick @user']
 handler.tags = ['group']
 handler.command = ['kick', 'expulsar'] 

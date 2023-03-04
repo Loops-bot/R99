125aa27286586048116bbd848a5584c9f3a3a037
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-03 21:57:04 -0600

    ⍣Loops ᴮᴼᵀ࿐

--- plugins/av-hola.js
+++ plugins/av-hola.js
@@ -5,9 +5,9 @@ let name = conn.getName(m.sender)
 let av = `./src/mp3/${pickRandom(["cri", "and"])}.mp3`
 
 conn.sendButton(m.chat, `Hola *${name}* \nMi creador no se encuentra en linea \n \nEscribele más tarde \n`, fgig, null, [
-     ['si'] 
+     ['Si', 'khajs'], ['No']
     ], m)
-conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+
 } 
 
 handler.customPrefix = /^(bro|jorge|jorgito|perro)$/i

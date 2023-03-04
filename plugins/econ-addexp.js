1cdf708e27c63d9e63e8fb85d7148c3212eab118
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-04 09:36:57 -0600

    ⍣Loops ᴮᴼᵀ࿐

---
+++ plugins/econ-addexp.js
@@ -0,0 +1,30 @@
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, text }) => {
+  let who
+  if (m.isGroup) who = m.mentionedJid[0]
+  else who = m.chat
+  if (!who) throw '✳️ Taguea al usuario'
+  let txt = text.replace('@' + who.split`@`[0], '').trim()
+  if (!txt) throw '✳️ Ingrese la cantidad de *coins* que quiere añadir'
+  if (isNaN(txt)) throw ' 🔢 sólo números'
+  let exp = parseInt(txt)
+  let exp = exp
+  
+  if (exp < 1) throw '✳️ Mínimo es  *1*'
+  let users = global.db.data.users
+  users[who].exp += exp
+
+  await m.reply(`≡ *EXPERIENCIA AÑADIDO*
+┌──────────────
+▢  *Total:* ${exp}
+└──────────────`)
+ conn.fakeReply(m.chat, `▢ Recibiste \n\n *+${exp} exp*`, who, m.text)
+}
+
+handler.help = ['addexp <@user>']
+handler.tags = ['econ']
+handler.command = ['addexp'] 
+handler.rowner = true
+
+export default handler

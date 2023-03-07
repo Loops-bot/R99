2895ae6d6611d1afece3fff1a0a93a12322af30e
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-06 20:17:07 -0600

    ⍣Loops ᴮᴼᵀ࿐

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -155,7 +155,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     
     let pp = './src/fg_logo.jpg'
     
-    conn.sendButton(m.chat, text.trim(), '*▢ Loops* ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/loopsbot_r98 saludo,', pp, [
+    conn.sendButton(m.chat, text.trim(), '▢ Hola Bienvenido al menu\n*▢ Loops* ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/loopsbot_r98', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
        ['⌬ Grupos', `${_p}gpdylux`]
         ],m, rpl)

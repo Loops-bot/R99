425424c4e665fcad1a622fa00b6704302ea08ad6
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-06 21:58:29 -0600

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
   
 ▢ *📌Titulo* : ${title}
 ▢ *⚖️Tamaño* : ${size}
-`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
+`.trim(), m, false, { mimetype: 'audio/mpeg', asAudio: chat.useAudio })
 		m.react(done)
         } catch {
 			await m.reply(`❎ Error: no se pudo descargar el audio`)

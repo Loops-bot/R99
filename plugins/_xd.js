0cef5a7fb9b3c54cf61851e3b30d9a8f47349cc8
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-09 09:25:36 -0600

    Update _xd.js

--- plugins/_xd.js
+++ plugins/_xd.js
@@ -2,17 +2,13 @@ import { sticker } from '../lib/sticker.js'
 
 import axios from 'axios'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-	let img = (await axios.get('https://raw.githubusercontent.com/Loops-bot/fgmods/main/img/xd.json')).data
+    if (/^xd|XD|Xd|xD$/i.test(m.text) ) {
+        let handler = async (m, { conn, text, usedPrefix, command }) => {
+	let sticker = (await axios.get('https://raw.githubusercontent.com/Loops-bot/fgmods/main/img/xd.json')).data
     let stiker = await sticker(null, global.API(`${pickRandom(img)}`), global.packname, global.author)
     if (stiker) return await conn.sendFile(m.chat, stiker, 'sticker.webp', { asSticker: true }, m)
     throw stiker.toString()   
 }
-
-handler.customPrefix = /^(xd)$/i
-handler.command = new RegExp
-
-export default handler
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]

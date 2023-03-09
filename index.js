97ec944d034d43b93fec46318c0867d5b30af5ed
LOOPS <123416512+Loops-bot@users.noreply.github.com>
2023-03-08 20:56:31 -0600

    ⍣Loops ᴮᴼᵀ࿐

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Loops -  E99', {
+say('Loops -  r98', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @r99._`, {
+say(`'${name}' By @r98._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
